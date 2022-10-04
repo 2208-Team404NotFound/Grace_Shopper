f7cbde151f445cb1924a4e5219e1fe29eee3e504
hdgelman <hdg18@icloud.com>
2022-10-04 13:37:28 -0500

    Added: dropTables and createTables to db/seedData

--- db/seedData.js
+++ db/seedData.js
@@ -1,1 +1,32 @@
-const client = require('./client')
+const client = require('./client');
+
+const dropTables = async () => {
+    try {
+        console.log('Dropping all tables!');
+
+        await client.query(`
+        DROP TABLE IF EXISTS users;
+        `);
+
+        console.log('Finished dropping tables!');
+    } catch (error) {
+        console.error(`Error dropping tables: ${error}`);
+    }
+}
+
+const createTables = async () => {
+    try {
+        console.log('Starting to build tables!');
+
+        await client.query(`
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(23) UNIQUE NOT NULL,
+            password VARCHAR(23) NOT NULL
+        );
+        `)
+
+    } catch (error) {
+        console.error(`Error building tables: ${error}`)
+    }
+}
--- router/index.js
+++ router/index.js
@@ -3,4 +3,11 @@ const router = express.Router();
 // const {  } = require('../db');
 const jwt = require('jsonwebtoken');
 
+router.use((error, req, res) => {
+    res.send({
+        name: error.name,
+        message: error.message
+    });
+});
+
 module.exports = router;
