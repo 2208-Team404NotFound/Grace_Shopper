a1ac3b5c72beddbcf22728a7a7d6990e0ab6b413
hdgelman <hdg18@icloud.com>
2022-10-12 13:39:21 -0500

    Added: route that should get all orders

--- api/orders.js
+++ api/orders.js
@@ -1,1 +1,18 @@
 const express = require('express');
+const ordersRouter = express.Router();
+const {
+    getAllOrders,
+    createOrders,
+    getOrdersById,
+    getOrdersByUserId
+} = require('../db');
+
+ordersRouter.get('/', async (req, res) => {
+    try {
+        const allOrders = await getAllOrders();
+
+        res.send(allOrders);
+    } catch (error) {
+        throw error;
+    }
+});
