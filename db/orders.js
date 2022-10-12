87e57629381b61cd074114ea9bfbf680061f3f3f
hdgelman <hdg18@icloud.com>
2022-10-12 12:05:34 -0500

    Added: getOrdersByUserId to db/orders

--- db/orders.js
+++ db/orders.js
@@ -1,4 +1,5 @@
-const client = require('./client');
+const ordersRouter = require('../api')
+const client = require('./client')
 
 const getAllOrders = async () => {
     try {
@@ -26,19 +27,35 @@ const createOrders = async ({ user_id, price, is_active }) => {
     }
 };
 
-// const getOrdersByOrderId = async (order_id) => {
-//     try {
-//         const { rows: [orders] } = await client.query(`
-//         SELECT orders.*
-//         `)
+const getOrdersById = async (id) => {
+    try {
+        const { rows: [orders] } = await client.query(`
+        SELECT orders * FROM orders
+        WHERE id=$1;
+        `, [id]);
 
-//     } catch (error) {
-//         throw error;
-//     }
-// }
+        return orders;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const getOrdersByUserId = async ({ user_id }) => {
+    try {
+        const { rows: orders } = await client.query(`
+        SELECT * FROM orders
+        WHERE user_id=$1;
+        `, [user_id]);
+
+        return orders;
+    } catch (error) {
+        throw error;
+    }
+};
 
 module.exports = {
     getAllOrders,
     createOrders,
-    // getOrdersByOrderId
+    getOrdersById,
+    getOrdersByUserId
 };
