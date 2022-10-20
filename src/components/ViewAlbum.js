47cc9141e100c6c0fce522a0becd2f5ab219e26e
Mark Boyd <markotheshark@gmail.com>
2022-10-19 22:11:49 -0500

    Added: Tracklist and label to ViewAlbum.js

--- src/components/ViewAlbum.js
+++ src/components/ViewAlbum.js
@@ -9,6 +9,26 @@ export const ViewAlbum = () => {
             <p>Album name: 333</p>
             <p>Year: 2020</p>
             <p>Album price: $9.99</p>
+            <p>Label: YEAR0001</p>
+            <p>Track list: </p>
+            <ol>
+                <li>Wings in Motion</li>
+                <li>Don't Worry</li>
+                <li>Keys to the City</li>
+                <li>Hero of My Story 3style3</li>
+                <li>100s</li>
+                <li>Mean Girls</li>
+                <li>Innocent of All Things</li>
+                <li>Reality Surf</li>
+                <li>Noblest Strive</li>
+                <li>It Girl</li>
+                <li>Oh Well</li>
+                <li>Valerie</li>
+                <li>Finder</li>
+                <li>Exstasia</li>
+                <li>Only One</li>
+                <li>Swan Lake</li>
+            </ol>
             
         </div>
     )
