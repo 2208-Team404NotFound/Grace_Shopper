b42f440f800e9d52bb9074d74151e5713a133ae1
GrungeElFz <100631908+GrungeElFz@users.noreply.github.com>
2022-10-20 04:33:22 -0500

    Dummy Shopping Cart (#51)
    * Initialized: A dummy cart page
    
    * Checkout --> Delivery options, Total Price, Checkout panel
    Modified: Added dummy items in the cart, Assigned classes  (src/components/Cart.js)
    Modified: Resized dummy album-cover items  (public/style.css)
    
    * Checkout --> Styling
    Modified: Added Delivery options, Total Price, Checkout pane  (src/components/Cart.js)
    Refactored: Deleted unncessary elements  (src/components/Cart.js)
    
    * Modified: Added Glassmorphism design for checkout tiles  (public/style.css)
    Refactored: Added the 3rd album, Added 3 outer divs, Assigned additional classes, Deleted unncessary elements  (src/components/Cart.js)

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import { Login } from './Login.js';
 import { Register } from './Register.js';
 import { Albums } from './Albums.js';
 import { Orders } from './Orders.js';
+import { Cart } from './Cart.js';
 
 
-export { Login, Register, Albums, Orders };
+export { Login, Register, Albums, Orders, Cart };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { useEffect, useState } from 'react';
-import { Login, Register, Albums } from "./components"
+import { Login, Register, Albums, Cart } from "./components"
 import { BrowserRouter, Route, Link, Routes } from 'react-router-dom'
 
 import { CgShoppingCart } from 'react-icons/cg';
@@ -29,7 +29,7 @@ const App = () => {
                     <Link id='login' to="Login"> Login </Link>
                     <Link id='register' to="Register"> Register </Link>
                     <Link id='albums' to="Albums"> Albums </Link>
-                    <Link id='shopping-cart' to="Orders"> <CgShoppingCart /> </Link>
+                    <Link id='shopping-cart' to="Cart"> <CgShoppingCart /> </Link>
                 </header>
 
                 <main>
@@ -37,7 +37,7 @@ const App = () => {
                         <Route path="Login" element={<Login setToken={setToken} />} />
                         <Route path="Register" element={<Register />} />
                         <Route path="Albums" element={<Albums token={token} />} />
-                        <Route path="View-album" element={<ViewAlbum />} />
+                        <Route path="Cart" element={<Cart />} />
                     </Routes>
                 </main>
 
